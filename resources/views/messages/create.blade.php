f4c510da56261abeac57ef2cbe1c615d91bafbed
jarleby <sebbe@jarleby.se>
2019-05-22 11:08:47 +0200

    la till en notification svg

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="messages"></div>
+    <div class="messages">
+        <div class="card"></div>
+    </div>
     <form action="">
         <div class="form-group">
             <label for="message">
                 Message:
             </label>
             <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Send" class="btn btn-primary">
         </div>
     </form>
 @endsection
